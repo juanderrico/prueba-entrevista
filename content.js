47addf9fc353167841cafd1730f4b6758e007c71
juan derrico <juancruzderrico@hotmail.com>
2022-04-22 22:15:45 -0300

    cambios al diseño en html y mostrar datos de la persona desde js

--- content.js
+++ content.js
@@ -9,9 +9,11 @@ class Persona{
 var boton= document.getElementById('boton-agregar');
 boton.addEventListener('click',crearPerfil);
 var baseDeDatos=[];
-
+baseDeDatos.push(new Persona("Juan", 40140041))
   function crearPerfil(){
     var nombre=document.getElementById("input-nombre").value;
     var dni = document.getElementById("input-dni").value;
       baseDeDatos.push(new Persona(nombre,dni));
   }
+var parrafo= document.getElementById("datos");
+parrafo.innerText = "Nombre: " + baseDeDatos[0].nombre +"  DNI: "+ baseDeDatos[0].dni;
