6141766c634d987b58a9d3646175217823ad21ef
juan derrico <juancruzderrico@hotmail.com>
2022-04-22 22:25:18 -0300

    ligeros cambios al diseño

--- content.js
+++ content.js
@@ -16,4 +16,4 @@ baseDeDatos.push(new Persona("Juan", 40140041))
       baseDeDatos.push(new Persona(nombre,dni));
   }
 var parrafo= document.getElementById("datos");
-parrafo.innerText = "Nombre: " + baseDeDatos[0].nombre +"  DNI: "+ baseDeDatos[0].dni;
+parrafo.innerText = "Nombre: " + baseDeDatos[0].nombre +" ------- DNI: "+ baseDeDatos[0].dni;
