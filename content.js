654fb1d187c8e016e841da997a1c70b164f1fadc
juan derrico <juancruzderrico@hotmail.com>
2022-04-25 14:25:50 -0300

    datos ahora se actualizan al agregar una nueva persona, pero se repiten

--- content.js
+++ content.js
@@ -14,6 +14,37 @@ baseDeDatos.push(new Persona("Juan", 40140041))
     var nombre=document.getElementById("input-nombre").value;
     var dni = document.getElementById("input-dni").value;
       baseDeDatos.push(new Persona(nombre,dni));
+      cargarDatos();
+
   }
-var parrafo= document.getElementById("datos");
-parrafo.innerText = "Nombre: " + baseDeDatos[0].nombre +" ------- DNI: "+ baseDeDatos[0].dni;
+
+function crearLinea(datos){
+  var holder=document.getElementById("holder");
+  var div1= document.createElement("div");
+  holder.appendChild(div1);
+  div1.classList.add("linea")
+  var parrafo=document.createElement("p");
+  parrafo.classList.add("datos")
+  div1.appendChild(parrafo);
+  var div2= document.createElement("div");
+  div1.appendChild(div2);
+  var botonEditar= document.createElement("button");
+  div2.appendChild(botonEditar);
+  botonEditar.classList.add("editar-boton")
+  var imagenEditar= document.createElement("img");
+    imagenEditar.classList.add("editar-imagen")
+  botonEditar.appendChild(imagenEditar)
+  var botonEliminar= document.createElement("button");
+  botonEliminar.classList.add("eliminar-boton")
+  div2.appendChild(botonEliminar);
+  var imagenEliminar= document.createElement("img");
+      imagenEliminar.classList.add("eliminar-imagen")
+        botonEliminar.appendChild(imagenEliminar)
+parrafo.innerText="Nombre: "+datos.nombre+"--------- DNI: "+datos.dni
+}
+function cargarDatos(){
+  for(var i=0;i<baseDeDatos.length;i++){
+    crearLinea(baseDeDatos[i]);
+  }
+}
+cargarDatos();
